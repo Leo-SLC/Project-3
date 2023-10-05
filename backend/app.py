e260c64fdab53a0bc2b2fe7372a4bc915bc0582e
Leo-SLC <leoantinozzi@Leos-MacBook-Pro.local>
2023-10-04 19:48:10 -0600

    Pushing Updated Code to New Branch

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,8 @@
 from flask import Flask, request, jsonify 
 
-
 app = Flask(__name__)
 
-@app.route('/api/imdb', methods=['GET'])
+@app.route('/', methods=['GET'])
 def api_get_all_imdb():
    # return jsonify(get_all_data()) # This is expecting json and returns json
    return get_all_data() # returns csv file, trying to avoid parcing json to csv because my front end expects csv
@@ -11,6 +10,6 @@ def api_get_all_imdb():
 if __name__ == "__main__":
     #app.debug = True
     #app.run(debug=True)
-    app.run() #run app
+    app.run(port=8000, debug=True) #run app
 
 
--- backend/database.py
+++ backend/database.py
@@ -1,45 +1,62 @@
 import csv
 import sqlite3
 import os
+from flask import Flask, request, jsonify
 
-print (os.getcwd())
 
-# connects to the sqlite database file imdb_top.db
+# Check the current working directory
+print(os.getcwd())
+
+# Connects to the SQLite database file imdb_top.db
 def connect_to_db():
     conn = sqlite3.connect('backend/imdb_top.db')
     return conn
 
-# getting all the column names from imdb top 1000 to be used when writing to output.csv
+# Getting all the column names from imdb top 1000 to be used when writing to output.csv
 def get_column_names():
     column_names = []
     try:
         conn = connect_to_db()
-        conn.row_factory = sqlite3.Row
         cursor = conn.cursor()
-        column_names = cursor.execute("SELECT name FROM sys.columns WHERE object_id = OBJECT_ID(/'imdbTop1000/')")
+        cursor.execute("PRAGMA table_info(imdbTop1000)")
+        columns = cursor.fetchall()
+        column_names = [column[1] for column in columns]
 
-    except:
-        column_names = []
+    except Exception as e:
+        print("Error getting column names:", str(e))
 
     return column_names
 
-# getting all data from imdb top 1000 table and wrting it to output.csv file
+# Getting all data from imdb top 1000 table and returning it as JSON
 def get_all_data():
-    try: 
+    try:
         conn = connect_to_db()
         cursor = conn.cursor()
-        data = cursor.execute('SELECT * FROM imdbTop1000')
-        
-        with open('backend/output.csv', 'w', newline='') as f:
-            writer = csv.writer(f)
-            writer.writerow(get_column_names())
-            writer.writerows(data)
+        cursor.execute('SELECT * FROM imdbTop1000')
+        column_names = get_column_names()
+        data = cursor.fetchall()
+        json_data = []
+        for row in data:
+            row_dict = {}
+            for i in range(len(column_names)):
+                row_dict[column_names[i]] = row[i]
+            json_data.append(row_dict)
 
-    except:
-        f = open('file.txt', 'r+')
-        f.truncate(0) # need '0' when using r+
-
-        print('output.csv')
-    return open('output.csv', 'r')
-
-get_all_data()
+        return json_data
+    
+    except Exception as e:
+        print("Error getting data:", str(e))
+        return []
+    
+    
+app = Flask(__name__)
+@app.route('/', methods=['GET'])
+def api_get_all_imdb():
+    try:
+        json_data = get_all_data()
+        return jsonify(json_data)
+    except Exception as e:
+        print("Error in API:", str(e))
+        return jsonify({'error': 'An error occurred'}), 500
+if __name__ == "__main__":
+    app.run(port=8000, debug=True)
