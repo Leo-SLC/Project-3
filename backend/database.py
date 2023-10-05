894379488c5235e1bb1f593665635e6741451cc3
Leo-SLC <leoantinozzi@Leos-MacBook-Pro.local>
2023-10-04 18:47:55 -0600

    Project Work Update

---
+++ backend/database.py
@@ -0,0 +1,45 @@
+import csv
+import sqlite3
+import os
+
+print (os.getcwd())
+
+# connects to the sqlite database file imdb_top.db
+def connect_to_db():
+    conn = sqlite3.connect('backend/imdb_top.db')
+    return conn
+
+# getting all the column names from imdb top 1000 to be used when writing to output.csv
+def get_column_names():
+    column_names = []
+    try:
+        conn = connect_to_db()
+        conn.row_factory = sqlite3.Row
+        cursor = conn.cursor()
+        column_names = cursor.execute("SELECT name FROM sys.columns WHERE object_id = OBJECT_ID(/'imdbTop1000/')")
+
+    except:
+        column_names = []
+
+    return column_names
+
+# getting all data from imdb top 1000 table and wrting it to output.csv file
+def get_all_data():
+    try: 
+        conn = connect_to_db()
+        cursor = conn.cursor()
+        data = cursor.execute('SELECT * FROM imdbTop1000')
+        
+        with open('backend/output.csv', 'w', newline='') as f:
+            writer = csv.writer(f)
+            writer.writerow(get_column_names())
+            writer.writerows(data)
+
+    except:
+        f = open('file.txt', 'r+')
+        f.truncate(0) # need '0' when using r+
+
+        print('output.csv')
+    return open('output.csv', 'r')
+
+get_all_data()
---
+++ frontend/StarterCode/static/js/client.js
@@ -0,0 +1,15 @@
+// Putting all of my API request logic in this file
+
+import 'regenerator-runtime/runtime'; //avoid a “regeneratorRuntime is not defined” error when using await and async
+import axios from 'axios';
+
+export {getFullName};
+
+const BASE_URL = '';
+
+//request the desired dataset from the API
+async function getDataset() { //async allows your code to run in the background without blocking the execution of other code.
+    try {
+        const response = await axios.get(`${BASE_URL}/`) //The await operator is used to wait for a Promise and get its fulfillment value. axios.get returns a promise
+
+        const dataset = response.data; //The axios response schema contains data, status, statusText, headers, congif, and request. Data is the response
