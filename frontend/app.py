2de8a37dacfad048ae67f7ba0499c205ad2a9fb0
Leo-SLC <leoantinozzi@Leos-MacBook-Pro.local>
2023-10-11 13:31:34 -0600

    Corrected Front end

---
+++ frontend/app.py
@@ -0,0 +1,54 @@
+import numpy as np
+import os
+
+import sqlite3
+import json
+
+from flask import Flask, jsonify, render_template
+
+
+#################################################
+# Flask Setup
+#################################################
+app = Flask(__name__)
+
+#################################################
+# Flask Routes
+#################################################
+def get_sql_conn():
+    conn = sqlite3.connect('test.db')
+    conn.row_factory = sqlite3.Row
+    return conn
+
+def row_to_dict(row):
+    return dict(row)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/movies")
+def names():
+    conn = get_sql_conn()
+    results = conn.execute('SELECT * FROM imdbTop1000').fetchall()
+    results_dict = [row_to_dict(row) for row in results]
+    conn.close()
+    return json.dumps(results_dict)
+
+    
+    # # Create our session (link) from Python to the DB
+    # session = Session(engine)
+
+    # """Return a list of all passenger names"""
+    # # Query all passengers
+    # results = session.query(Movies.unique_id).all()
+
+    # session.close()
+
+
+
+    # return (result
+
+
+if __name__ == '__main__':
+    app.run(port=8000,debug=True)
--- frontend/static/js/app.js
+++ frontend/static/js/app.js
@@ -1,17 +1,6 @@
-d3.json('/movies').then((data)=>{console.log({data})});
-
-let array = [d3.json('/movies')];
+d3.json('/movies').then((data) => {console.log(data)})
 
 
-<<<<<<< Updated upstream
-d3.json('/movies').then((data) => { 
-    //console.log(JSON.stringify(data));
-    let newdata = JSON.parse(JSON.stringify(data));
-    console.dir(newdata);
-    console.log(newdata);
- 
-    
-=======
 function init() {
 
     // Use d3.json to load the data
@@ -128,106 +117,11 @@ function init() {
 function optionChanged(year) {
     buildCharts(year, "");
   }
->>>>>>> Stashed changes
 
   init();
 
-    //let newjsondata = d3.json();
-    
-    // d3.json(newdata).then(({ Genres }) => {
-    //     Genres.forEach((Genre) => {
-    //       d3.select("select").append("option").text(Genre);
-    //     });
-    //     optionChanged();
+init();
+// function generateChart(data) {
+// // Chart code using data
+// }
 
-   
-    //let mappednewdata = new Map(Object.entries(JSON.parse(newdata)));
-
-    //let mappednewdata = new Map(Object.entries(JSON.parse(newdata)));
-
-    //console.log(mappednewdata);
-});
-
-
-//movie_data = d3.json('/movies');
-
-//let json_data = d3.json('/movies').then((data));
-
-//movie_data = new Map(Object.entries(JSON.parse(d3.json('/movies'))));
-
-//console.log(movie_data)
-
-// genre = movie_data.organizations.map(function(d) { return d.genre; });
-
-// var Genre = d3.map(movie_data, function(d) {
-//     return d.Genre;
-//  });
-
-
-// mappednewdata => {
-//     data.forEach((key) => {
-//       d3.select("select").append("option").text({Genre});
-//     });
-//     optionChanged();
-//   };
-  
-//   const optionChanged = () => {
-//     let choice = d3.select("select").node().value;
-  
-//     d3.json('/movies').then(({ metadata, samples }) => {
-//       let meta = metadata.filter((obj) => obj.id == choice)[0];
-//       let sample = samples.filter((obj) => obj.id == choice)[0];
-  
-//       d3.select(".panel-body").html("");
-  
-//       Object.entries(meta).forEach(([key, val]) => {
-//         d3.select(".panel-body")
-//           .append("h4")
-//           .text(`${key.toUpperCase()}: ${val}`);
-//       });
-  
-//       const{otu_ids, otu_labels, sample_values} = sample;
-  
-//       var data = [
-//           {
-//             x: sample_values.slice(0, 10).reverse(),
-//             y: otu_ids.slice(0, 10).reverse().map(y => `OTU ${y}`),
-//             text: otu_labels.slice(0,10).reverse(),
-//             type: 'bar',
-//             orientation:"h"
-//           }
-//         ];
-        
-//         Plotly.newPlot('bar', data); 
-        
-//         var trace1 = {
-//           x: otu_ids,
-//           y: sample_values,
-//           text: otu_labels,
-//           mode: 'markers',
-//           marker: {
-//             size: sample_values,
-//             color: otu_ids,
-//             colorscale: "Earth"
-//           }
-//         };
-        
-//         var data = [trace1];
-        
-//         Plotly.newPlot('bubble', data);
-  
-//         var data = [
-//           {
-//             domain: { x: [0, 1], y: [0, 1] },
-//             value: meta.wfreq,
-//             title: { text: "<b>Belly Button Washing Frequency</b> <br> Scrubs per week"},
-//             type: "indicator",
-//             mode: "gauge+number",
-//             delta: { reference: 400 },
-//             gauge: { axis: { range: [null, 9] } }
-//           }
-//         ];
-//         Plotly.newPlot('gauge', data);
-  
-//     });
-//   };
